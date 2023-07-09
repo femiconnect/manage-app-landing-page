8abe045795017667ce8ddaf1e6e2f60df31add89
femi-connect <60244053+femiconnect@users.noreply.github.com>
2023-07-09 19:04:32 +0100

    created mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/images/logo.svg';
 
 const Navbar = () => {
+   const [nav, setNav] = useState(false);
+
+   const handleNav = () => {
+      setNav(!nav);
+   };
+
    return (
       <nav className='container py-6'>
          {/* -- Flex container -- */}
@@ -31,7 +37,65 @@ const Navbar = () => {
             </ul>
 
             {/* --nav button -- */}
-            <button className='btn'>Get Started</button>
+            <button className='hidden md:block md:btn'>Get Started</button>
+
+            {/* -- Mobile Navbar -- */}
+
+            {/* --hamburger-- */}
+            <button
+               onClick={handleNav}
+               className='block relative w-6 h-6 transition-all cursor-pointer z-10 focus:outline-none md:hidden'
+            >
+               <span
+                  className={
+                     nav
+                        ? 'absolute top-0 left-0 w-6 h-[2px] bg-white delay-300'
+                        : 'absolute top-0 left-0 w-6 h-[2px] bg-gray-900 delay-300'
+                  }
+               ></span>
+               <span
+                  className={
+                     nav
+                        ? 'absolute top-0 left-0 w-6 h-[2px] bg-white translate-y-[7px] delay-300'
+                        : 'absolute top-0 left-0 w-6 h-[2px] bg-gray-900 translate-y-[7px] delay-300'
+                  }
+               ></span>
+               <span
+                  className={
+                     nav
+                        ? 'absolute top-0 left-0 w-6 h-[2px] bg-white translate-y-[14px] delay-300'
+                        : 'absolute top-0 left-0 w-6 h-[2px] bg-gray-900 translate-y-[14px] delay-300'
+                  }
+               ></span>
+            </button>
+
+            {/* -- mobile menu -- */}
+            <div
+               onClick={handleNav}
+               className={
+                  nav
+                     ? 'overflow-y-hidden fixed left-0 top-0 w-full h-screen bg-black/95 px-4 py-7 flex flex-col text-gray-300 font-bold ease duration-300 md:hidden'
+                     : 'fixed top-0 left-[-100%] h-screen ease duration-300'
+               }
+            >
+               <ul className='w-full h-full flex flex-col justify-center items-center space-y-10'>
+                  <li className='hover:text-brightRed cursor-pointer'>
+                     Pricing
+                  </li>
+                  <li className='hover:text-brightRed cursor-pointer'>
+                     Product
+                  </li>
+                  <li className='hover:text-brightRed cursor-pointer'>
+                     About Us
+                  </li>
+                  <li className='hover:text-brightRed cursor-pointer'>
+                     Careers
+                  </li>
+                  <li className='hover:text-brightRed cursor-pointer'>
+                     Community
+                  </li>
+               </ul>
+            </div>
          </div>
       </nav>
    );
